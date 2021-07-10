e84c9582ebf6706aa0fb411a4996e38c6af77676
Apurba Panja <apurbapanja1@gmail.com>
2021-07-10 19:31:07 +0530

    Added comments

--- java/DCP001TwoSum.java
+++ java/DCP001TwoSum.java
@@ -1,6 +1,15 @@
 import java.util.HashSet;
 
 public class DCP001TwoSum {
+  /*
+   * Given a list of numbers and a number k, return whether any two numbers from
+   * the list add up to k.
+   * 
+   * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is
+   * 17.
+   * 
+   * Bonus: Can you do this in one pass?
+   */
 
   public static boolean twoSum(int[] nums, int target) {
     HashSet<Integer> seen = new HashSet<>();
@@ -9,7 +18,7 @@ public class DCP001TwoSum {
       if (seen.contains(nums[i])) {
         return true;
       } else {
-        // Storing the second number which can be added 
+        // Storing the second number which can be added
         // with the current number to add up to the target
         seen.add(target - nums[i]);
       }
