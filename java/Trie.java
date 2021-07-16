44622f0934e188c6ab0027e2e9b9bf7b26eb4d83
Apurba Panja <apurbapanja1@gmail.com>
2021-07-16 18:05:44 +0530

    Updated Trie

--- java/Trie.java
+++ java/Trie.java
@@ -1,18 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Trie {
-  private class TrieNode {
-    Map<Character, TrieNode> children;
-    boolean endOfWord;
-
-    public TrieNode() {
-      children = new HashMap<>();
-      endOfWord = false;
-    }
-  }
-
-  private final TrieNode root;
+  public final TrieNode root;
 
   public Trie() {
     root = new TrieNode();
