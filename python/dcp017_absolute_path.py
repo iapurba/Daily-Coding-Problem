4e7db113bcb2c394e8d99e1b52bc13e09bc5bfdc
Apurba Panja <apurbapanja1@gmail.com>
2021-07-22 16:05:40 +0530

    Problem #17 longest absolute file path

--- python/dcp017_absolute_path.py
+++ python/dcp017_absolute_path.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python3
+
+
 def build_fs(input):
     fs = {}
-    files = input.split('\n')
+    files = input.split("\n")
 
     current_path = []
+
     for f in files:
         indentation = 0
-        while '\t' in f[:2]:
+        while "\t" in f[:2]:
             indentation += 1
             f = f[1:]
 
         current_node = fs
+
         for subdir in current_path[:indentation]:
             current_node = current_node[subdir]
 
@@ -23,4 +28,30 @@ def build_fs(input):
 
     return fs
 
-print(build_fs("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"))
+
+def longest_path(fs):
+    paths = []
+    for key, val in fs.items():
+        if val == True:
+            paths.append(key)
+        else:
+            paths.append(key + '/' + longest_path(val))
+
+    paths = [path for path in paths if '.' in path]
+
+    if paths:
+        return max(paths, key=lambda path: len(path))
+    else:
+        return ''
+
+
+def longest_absolute_path(s):
+    return(longest_path(build_fs(s)))
+
+
+if __name__ == "__main__":
+    s1 = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+    s2 = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+
+    print(longest_absolute_path(s1))
+    print(longest_absolute_path(s2))
